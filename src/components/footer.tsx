0c39f4a93210b953abe7001bab1baa240467267b
noelmagondim <noelmagondim@gmail.com>
2025-01-27 15:13:58 -0300

    Criando componentes header e footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,5 @@
+function Footer() {
+  return <footer>Criando o Footer</footer>
+}
+
+export default Footer;
---
+++ src/components/header.tsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <h1>Criando o Header</h1>
+}
+
+export default Header;
