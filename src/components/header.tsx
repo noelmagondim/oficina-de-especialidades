4a47ca0d29bd3a6002be714054805b8e61795187
noelmagondim <noelmagondim@gmail.com>
2025-01-27 22:10:17 -0300

    criando o Header.css

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '/src/styles/Header.css'; // Importando o arquivo CSS
+
 const Header: React.FC = () => {
-  return <h1>Criando o Header</h1>
+  return (
+    <header className="header-container">
+      <div className="logo">
+        <Link to="/" className="logo-link">
+          <h1 className="logo-text">Feira de Especialidades 2025.1</h1>
+        </Link>
+      </div>
+      <nav className="navigation">
+        <Link to="/" className="nav-link">Início</Link>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
