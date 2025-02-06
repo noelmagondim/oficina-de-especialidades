d667509b1733dff1f5c94b630690c3af852f8b3c
noelmagondim <noelmagondim@gmail.com>
2025-02-06 07:25:39 -0300

    Retirando o vercel do projeto

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Definindo a configuração do Vite para o frontend
 export default defineConfig({
   plugins: [react()],
+  root: './frontend', // Certificando-se de que o Vite procure a pasta frontend
+  build: {
+    outDir: '../dist', // Para definir a saída do build
+  },
 })
+
